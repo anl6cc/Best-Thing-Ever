315e16f4986228cc1c0ec9c040189a7305b2d1af
Alexander Luu <anl6cc@virginia.edu>
2014-04-12 16:05:17 -0400

    more colors and grid

--- paint_demo.py
+++ paint_demo.py
@@ -2,27 +2,64 @@ from Tkinter import *
 
 b1 = "up"
 xold, yold = None, None
-color = 'blue'
+color = 'black'
 
 def main():
     root = Tk()
-    root.button = Button(root,text="Red",command=red)
-    root.button.pack(side="top")
-    root.button = Button(root,text="Black",command=black)
-    root.button.pack(side="top")
+#    Label(bg='#234').grid(row=1,column=1)
+    btn = Button(root,text='black',command=black,bg='#234').grid(row=1,column=1)
+ #   btn.configure(bg='blue')
+    Button(root,text="Red",command=red).grid(row=1,column=2)
+    Button(root,text="Blue",command=blue).grid(row=1,column=3)
+    Button(root,text="Green",command=green).grid(row=1,column=4)
+    Button(root,text="Yellow",command=yellow).grid(row=2,column=1)
+    Button(root,text="Orange",command=orange).grid(row=2,column=2)
+    Button(root,text="Purple",command=purple).grid(row=2,column=3)
+    Button(root,text="Eraser",command=white).grid(row=2,column=4)
     drawing_area = Canvas(root)
-    drawing_area.pack()
+    drawing_area.grid(row=3,column=1,columnspan=4,rowspan=4);
     drawing_area.bind("<Motion>", motion)
     drawing_area.bind("<ButtonPress-1>", b1down)
     drawing_area.bind("<ButtonRelease-1>", b1up)
     root.mainloop()
 
+def black():
+    global color
+    color = 'black'
+
 def red():
-    print("LOL")
+    global color
     color = 'red'
 
-def black():
-    color = 'black'
+def blue():
+    global color
+    color = 'blue'
+
+def green():
+    global color
+    color = 'green'
+
+def yellow():
+    global color
+    color = 'yellow'
+
+def orange():
+    global color
+    color = 'orange'
+
+def purple():
+    global color
+    color = 'purple'
+
+def white():
+    global color
+    color = 'white'
+
+def changeColor(c):
+    global color
+    print(c);
+    color = c
+    print(color);
 
 def b1down(event):
     global b1
