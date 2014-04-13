05eeeeea660d795252cc38985142f6a4cf851d44
Alexander Luu <anl6cc@virginia.edu>
2014-04-12 20:11:54 -0400

    size BIG D

--- paint_demo.py
+++ paint_demo.py
@@ -3,6 +3,7 @@ from Tkinter import *
 b1 = "up"
 xold, yold = None, None
 color = 'black'
+size = 2
 
 def main():
     root = Tk()
@@ -11,7 +12,9 @@ def main():
         btn = Button(root, command=lambda c=colors[i]:changeColor(c), width=2,
                      bg=colors[i], activebackground=colors[i])
         btn.grid(row=i/4, column=96+i%4, sticky=E+W)
-    drawing_area = Canvas(root, width=400, height=300)
+    for i in range(4):
+        Button(root,command=lambda s=i*2:changeSize(s),width=2).grid(row=3, column=96+i%4, sticky=E+W)
+    drawing_area = Canvas(root, width=400, height=300, bg='white')
     drawing_area.grid(row=2, column=0, columnspan=100);
     drawing_area.bind("<Motion>", motion)
     drawing_area.bind("<ButtonPress-1>", b1down)
@@ -21,6 +24,10 @@ def main():
 def changeColor(c):
     global color
     color = c
+
+def changeSize(s):
+    global size
+    size = s
 
 def b1down(event):
     global b1
@@ -36,7 +43,7 @@ def motion(event):
     if b1 == "down":
         global xold, yold
         if xold is not None and yold is not None:
-            event.widget.create_line(xold, yold, event.x, event.y, fill=color, smooth=TRUE)
+            event.widget.create_line(xold, yold-size/2, event.x, event.y-size/2, fill=color, width=size, smooth=TRUE)
         xold = event.x
         yold = event.y
 
